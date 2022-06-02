aedcacc9bac4b6ae2c2e39752d8170266c964cc9
kingdavid <kshedu197@gmail.com>
2022-06-02 13:17:31 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - entery
  *
- * Description: i dont
+ * Description: i
  *
  * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return(1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
