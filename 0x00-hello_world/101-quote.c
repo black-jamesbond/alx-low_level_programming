0c28a5c72ac6052857f0edfa009fc80233ec5416
kingdavid <kshedu197@gmail.com>
2022-06-02 13:24:46 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - entery
  *
- * Description: i
+ * Description:q
  *
  * Return: 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
