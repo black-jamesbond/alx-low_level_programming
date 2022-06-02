939707bf1caeefa4c09ac1d0346d197f3b399a8c
kingdavid <kshedu197@gmail.com>
2022-06-02 09:30:51 -0700

    ok

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+ * main - entry point
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	printf("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
