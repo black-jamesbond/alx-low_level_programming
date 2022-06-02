6d0744148f328442af3ada2b24d106983dec0b7d
kingdavid <kshedu197@gmail.com>
2022-06-02 13:37:56 -0700

    o

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
  */
 int main(void)
 {
-fwrite(4, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 5);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
