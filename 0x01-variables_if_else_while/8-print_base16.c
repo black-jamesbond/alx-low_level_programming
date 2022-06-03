05066af180771c7d3cd15b04ba72f7b21da5e30e
kingdavid <kshedu197@gmail.com>
2022-06-03 11:23:57 -0700

    o

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main- nsdnds
+ *
+ * Description: khbsfbf
+ *
+ * Return: 0
+ *
+ */
+
+int main (void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a <10; a++)
+	{
+		putchar(a + '0');
+	}
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
