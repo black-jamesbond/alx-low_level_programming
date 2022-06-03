ad9f4cc24bf61a74931ece1625c3b2e9d73d5c63
kingdavid <kshedu197@gmail.com>
2022-06-03 10:27:23 -0700

    o

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main- code for all
+ *
+ * Description: print numbers without printf
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char a;
+	char ch;
+	char d;
+
+	ch = '\n';
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar(ch);
+	return (0);
+}
