9fa219c3fd7b0fb34985277b734ec4fcdfeede88
kingdavid <kshedu197@gmail.com>
2022-06-03 12:19:38 -0700

    o

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main- jknsdjknsjd
+ *
+ * Description: jksdkhd
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar( a + '0');
+		putchar(',');
+		putchar( );
+	}
+	return (0);
+}
+
