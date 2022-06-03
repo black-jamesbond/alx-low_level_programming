099db689e3aeca7e2a1a368f39d6c6502c7345e7
kingdavid <kshedu197@gmail.com>
2022-06-03 07:41:14 -0700

    o

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - all codes
+ *
+ * Description:A program that prints alphabets in lowercase follwed by a line
+ *
+ * Return = 0
+ *
+ */
+
+int main(void)
+{
+	char alphabets;
+	char ch;
+
+	ch = '\n';
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	putchar(ch);
+	return (0);
+}
