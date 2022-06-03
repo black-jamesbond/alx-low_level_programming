b121e56818e3f4998c9f92ae995b5a68ab19f399
kingdavid <kshedu197@gmail.com>
2022-06-03 10:47:47 -0700

    o

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main- code for all
+ *
+ * Description: jkndcjndc
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alphabet;
+	char ch;
+
+	ch = '\n';
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar(ch);
+	return (0);
+}
