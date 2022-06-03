447e5e254b900a40575298d97c93743b18683084
kingdavid <kshedu197@gmail.com>
2022-06-03 09:02:02 -0700

    o

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main- code for all
+ *
+ * Description: print a to z
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	return (0);
+}
