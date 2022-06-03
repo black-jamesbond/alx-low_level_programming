ae3a5e9de6153da50fb32eaf5df115ef50c9b9ad
kingdavid <kshedu197@gmail.com>
2022-06-03 10:07:43 -0700

    o

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main- to check a con
+ *
+ * Description: it is ti compare
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	char ch;
+
+	ch = '\n';
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d",a);
+	}
+	putchar(ch);
+	return (0);
+}
+
