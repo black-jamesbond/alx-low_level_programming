16086c6e4ef9e41405ba9259b2eec7e2645f31d0
black-jamesbond <104460484+black-jamesbond@users.noreply.github.com>
2022-06-08 05:04:10 +0800

    xf
    xfg

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,26 @@
 #include "main.h"
+/**
+ *skeleton - reused to produce a comma and space(s) between
+ *each number in the times table.
+ *@n: the parameter n keeps track of when to put commas, spaces and the number
+ */
+void skeleton(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
 
 /**
  *times_table - printing the 9 times table starting with 0
@@ -28,25 +50,3 @@ void times_table(void)
 	}
 }
 
-/**
- *skeleton - reused to produce a comma and space(s) between
- *each number in the times table.
- *@n: the parameter n keeps track of when to put commas, spaces and the number
- */
-void skeleton(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-}
