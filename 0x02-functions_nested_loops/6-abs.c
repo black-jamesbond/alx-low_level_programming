9b716d25e8ab4eecac16ba67ff6afe350da8b553
kingdavid <kshedu197@gmail.com>
2022-06-07 13:35:13 -0700

    o

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs- jbsdkjf
+ *
+ * @r: snfljndfljn
+ *
+ * main- lksndf;ksmdfd
+ *
+ * Return: 0
+ *
+ */
+
+int _abs(int r)
+{
+	if (r > 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+	}
+}
