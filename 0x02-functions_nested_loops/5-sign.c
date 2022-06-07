a0470c32b1c82a866c716a486799f285efe716f3
kingdavid <kshedu197@gmail.com>
2022-06-07 12:54:03 -0700

    o

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.c"
+/**
+ * print_sign- print out 
+ *
+ * @n: value to check rhrn 
+ * Description: pritn
+ * Return: 0
+ *
+ */
+
+int print_sign(int n)
+{
+	int j = -1;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (j);
+	}
+	if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+}
