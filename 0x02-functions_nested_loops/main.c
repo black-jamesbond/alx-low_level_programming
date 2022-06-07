e7803dd5485b643b2c323a41c2531dc09eca85c8
kingdavid <kshedu197@gmail.com>
2022-06-07 13:14:16 -0700

    o

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -8,13 +8,29 @@
 
 int main(void)
 {
-	int r;
 
-	r = _islower('a');
+	int r;
+	
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _islower(20);
+	_putchar('\n');
+	r = print_sign(0);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
-	r = _islower(0);
+	_putchar('\n');
+	r = print_sign(0xff);
+	_putchar(',');
+	_putchar(' ');
 	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(-1);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+												    return (0);
 	return (0);
 }
