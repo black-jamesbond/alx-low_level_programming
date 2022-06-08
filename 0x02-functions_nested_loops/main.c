ab94d6e3d02e738b032947b22f3cb790d13db390
black-jamesbond <104460484+black-jamesbond@users.noreply.github.com>
2022-06-08 16:18:02 +0800

    Update main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,35 +1,18 @@
 #include "main.h"
+
 /**
- * main- check the code
- * Description: to print all alphabets in lower case from a to z
- * Return:Always 0.
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
-
 int main(void)
 {
-
-	int r;
-	
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-									return (0);
+    print_times_table(3);
+    _putchar('\n');
+    print_times_table(5);
+    _putchar('\n');
+    print_times_table(98);
+    _putchar('\n');
+    print_times_table(12);  
+    return (0);
 }
