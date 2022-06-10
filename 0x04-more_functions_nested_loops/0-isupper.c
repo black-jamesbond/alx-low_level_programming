3b90c91dcb69e2babe6d4424f8d94733ba8cc338
black-jamesbond <104460484+black-jamesbond@users.noreply.github.com>
2022-06-10 15:40:07 +0800

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character
+ * Return: 1 is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
+© 2022 GitHub, Inc.
