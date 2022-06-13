b2efa3a249f806def93d95f2455debff2db50548
kingdavid <kshedu197@gmail.com>
2022-06-13 12:30:59 -0700

    o

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * Reset_to_98 - set  the value in memory location to 98.
+ * reset_to_98 - set  the value in memory location to 98.
  * @n: parameter
  * Description: sdfhikdjf
  * Return: void.
