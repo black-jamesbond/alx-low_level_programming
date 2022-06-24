ec26462839b48aa0b992c4d6abb6ede58ee03ef6
kingdavid <kshedu197@gmail.com>
2022-06-24 08:19:46 -0700

    oo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- program for argc and argv.
+ *
+ * Description: it will print the number of aregumentd and more.
+ *
+ * aReturn: it will return 0.
+ */
+
+int main(int argc, char* argv[])
+{
+	int i=0;
+
+	printf("%s\n",argv[i]);
+	return (0);
+}
